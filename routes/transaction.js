fcad7badd98b0ccaae95f4c22f6a6f87e51372ba
Pablo-Coronel <82612803+Pablo-Coronel@users.noreply.github.com>
2021-11-23 16:36:22 -0300

    cambios en controllers, models y routes de transactions

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,18 +1,21 @@
-var express = require('express');
-
-var transactionController = require('../controllers/transaction');
-
+var express = require("express");
 var api = express.Router();
+var transactionController = require("../controllers/transaction");
+const ensureAuth = require("../middlewares/authentication");
 
-api.post("/transaction/:id", transactionController.addTransaction);
+api.post("/transaction", transactionController.createTransaction);
 
-api.get('/transaction/:id/:name', transactionController.getTransaction);
-
-api.delete('/transaction/:id', transactionController.delTransaction);
-
-api.put('/transaction/:id', transactionController.putTransaction);
-
-api.get('/transactions', transactionController.getTransactions);
-// api.delete('/transaction/:prop/:value')
+api.get("/movements", ensureAuth, transactionController.getMovements); //req.params.name o req.params.surname o req.params { name, surname }
+api.get(
+  "/movements-criteria/:criteria?",
+  ensureAuth,
+  transactionController.getMovementsByValue
+);
+api.put(
+  "/movements-update/:id",
+  ensureAuth,
+  transactionController.updateMovement
+);
+api.put("/fix", ensureAuth, transactionController.updateInvalidDate);
 
 module.exports = api;
